0dc60cf576d22f80cab050b1357729520a7c9c50
Henrique <hikoalbuquerque@gmail.com>
2026-04-09 17:24:16 -0300

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,5 +1,5 @@
 // Configuração de ambiente do frontend.
 // Em produção você pode sobrescrever com window.APP_CONFIG antes de carregar api.js.
 window.APP_CONFIG = window.APP_CONFIG || {
-  API_URL: ''
+  API_URL: 'https://promo-telegram-gateway-476120210909.southamerica-east1.run.app'
 };
