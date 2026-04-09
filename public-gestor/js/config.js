b296871195e50793cd64ae39095440df3855ece1
Henrique <hikoalbuquerque@gmail.com>
2026-04-09 16:26:22 -0300

    Update config.js

--- public-gestor/js/config.js
+++ public-gestor/js/config.js
@@ -2,7 +2,7 @@
 // Única fonte de verdade para URLs e constantes do painel do gestor
 
 const CONFIG = {
-  CLOUD_RUN_URL: 'https://promo-telegram-gateway-v3-476120210909.southamerica-east1.run.app',
+  CLOUD_RUN_URL: 'https://promo-telegram-gateway-476120210909.southamerica-east1.run.app',
   APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxsWriuYAFDkqiwDDoKpu0L34u5DGa23rIz4qwN6hLklxw3qnXQrbZXjbut0kSBe56N/exec',
 
   // Tipos de vínculo autorizados a acessar o painel gestor
